2b4a9f7adb86440e6a30e01c97496f0299dfeca2
Mike Harris <MikeMKH@users.noreply.github.com>
2023-08-15 06:22:11 -0500

    add ex 2.1

---
+++ ch02/hello.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#define TWO 2
+
+int main(int argc, char **argv)
+{
+  int result;
+  
+  printf("Hello world!\n");
+  
+  result = TWO * 4;
+  printf("2 * 4 = %d", result);
+  
+  return 0;
+}
---
+++ ch02/hello.s
@@ -0,0 +1,38 @@
+	.section	__TEXT,__text,regular,pure_instructions
+	.build_version macos, 11, 0	sdk_version 12, 1
+	.globl	_main                           ## -- Begin function main
+	.p2align	4, 0x90
+_main:                                  ## @main
+	.cfi_startproc
+## %bb.0:
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset %rbp, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register %rbp
+	subq	$32, %rsp
+	movl	$0, -4(%rbp)
+	movl	%edi, -8(%rbp)
+	movq	%rsi, -16(%rbp)
+	leaq	L_.str(%rip), %rdi
+	movb	$0, %al
+	callq	_printf
+	movl	$8, -20(%rbp)
+	movl	-20(%rbp), %esi
+	leaq	L_.str.1(%rip), %rdi
+	movb	$0, %al
+	callq	_printf
+	xorl	%eax, %eax
+	addq	$32, %rsp
+	popq	%rbp
+	retq
+	.cfi_endproc
+                                        ## -- End function
+	.section	__TEXT,__cstring,cstring_literals
+L_.str:                                 ## @.str
+	.asciz	"Hello world!\n"
+
+L_.str.1:                               ## @.str.1
+	.asciz	"2 * 4 = %d"
+
+.subsections_via_symbols
