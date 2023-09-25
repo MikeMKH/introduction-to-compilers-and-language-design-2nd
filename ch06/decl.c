0f1abb0296a14039b98ec86adfecf0f00800e0bd
Mike Harris <MikeMKH@users.noreply.github.com>
2023-09-25 06:32:03 -0500

    add 6.2

---
+++ ch06/decl.c
@@ -0,0 +1,28 @@
+#include "decl.h"
+#include "type.h"
+#include "expr.h"
+#include "stmt.h"
+#include <stdio.h>
+
+struct decl * decl_create(
+  char *name, struct type *type, struct expr *value, struct stmt *code, struct decl *next)
+{
+  struct decl *d = malloc(sizeof(*d));
+  d->name = name;
+  d->type = type;
+  d->value = value;
+  d->code = code;
+  d->next = next;
+}
+
+void decl_print(struct decl *d, int indent )
+{
+  if (!d) return;
+  
+  printf("%*s: ", indent, d->name);
+  type_print(d->type);
+  if (d->value) { printf(" = "); expr_print(d->value); }
+  if (d->code) { printf(" { "); stmt_print(d->code); printf(" }"); }
+  printf("\n");
+  decl_print(d->next, indent);
+}
